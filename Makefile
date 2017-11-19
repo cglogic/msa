6479b1657f275482300b34587c0607046abe3239
gor <gor@clogic.com.ua>
2017-11-19 17:45:56 +0200

    Start work

---
+++ Makefile
@@ -0,0 +1,35 @@
+TARGET=msa
+VERSION=0.1
+
+PREFIX?=/usr/local
+
+CPPFLAGS=-DVERSION=\"${VERSION}\"
+CFLAGS+=-Wall -Wextra -std=c11 -pedantic -I$(PREFIX)/include $(CPPFLAGS)
+LDFLAGS=-L$(PREFIX)/lib
+
+MANDIR?=$(PREFIX)/man/man1
+BINDIR?=$(PREFIX)/bin
+
+INSTALL=install
+INSTALL_PROGRAM=$(INSTALL)
+INSTALL_DATA=${INSTALL} -m 644
+
+LIBS=-lpthread -lncurses
+OBJECTS=src/main.o
+
+all: $(TARGET)
+
+$(TARGET): $(OBJECTS)
+	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ $(OBJECTS) $(LIBS)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c -o $@ $<
+
+clean:
+	rm -fv $(OBJECTS)
+	rm -fv $(TARGET)
+
+install: msa
+	mkdir -p $(DESTDIR)$(BINDIR)
+	cp $(TARGET) $(DESTDIR)$(BINDIR)/
+	chmod 755 ${DESTDIR}${BINDIR}/$(TARGET)
---
+++ src/main.c
@@ -0,0 +1,79 @@
+#include <syslog.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "config.h"
+
+struct url
+{
+	// Protocol
+	// Address
+	// Port
+};
+
+bool parse_url(const char *url)
+{
+	return false;
+}
+
+void options_parse(int argc, char** argv)
+{
+	int ch = 0;
+	const char *opt_short = "hv";
+	struct option opt_long[] =
+	{
+		{"help",	no_argument,	NULL,	'h'},
+		{"version",	no_argument,	NULL,	'v'},
+		{NULL,		0,		NULL,	0}
+	};
+
+	while ((ch = getopt_long(argc, argv, opt_short, opt_long, NULL)) != -1)
+	{
+		switch (ch)
+		{
+		case 'h':
+			printf("Usage: %s [OPTIONS] [URL]\n", argv[0]);
+			printf("  -v, --version             print version info and exit\n");
+			printf("  -h, --help                print this help and exit\n");
+			exit(0);
+			break;
+
+		case 'v':
+			printf("Media Stream Analizer version %s\n", VERSION);
+			break;
+
+		case '?':
+			//printf("Unknown option `-%c'.\n", optopt);
+			break;
+
+		default:
+			//syslog(LOG_ERR, "invalid option -- %c", ch);
+			syslog(LOG_ERR, "Try `%s --help' for more information.", argv[0]);
+			exit(0);
+		}
+	}
+
+	for (int index = optind; index < argc; index++)
+	{
+		printf("Check this URL: %s\n", argv[index]);
+
+		if (parse_url(argv[index]))
+			break;
+	}
+}
+
+int main(int argc, char** argv)
+{
+	openlog(strrchr(argv[0], '/') != NULL ? strrchr(argv[0], '/') + 1 : argv[0], LOG_CONS | LOG_PERROR, LOG_DAEMON);
+
+	options_parse(argc, argv);
+
+	// Start work
+
+	closelog();
+
+	return EXIT_SUCCESS;
+}
