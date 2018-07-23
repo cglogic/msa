47f6e5e91266f6affbf1b07ce1d94914845a3d20
gor <gor@clogic.com.ua>
2018-07-23 14:38:13 +0300

    Add run rule to Makefile

--- src/network.c
+++ src/network.c
@@ -1,11 +1,29 @@
 #include "network.h"
 
+#include <sys/socket.h>
+#include <stdlib.h>
+
 bool url_parse(const char *addr, struct url_t *url)
 {
-	return false;
+	url = malloc(sizeof(struct url_t));
+	url->network = PF_INET;
+	url->protocol = SOCK_DGRAM;
+	url->address = atoi("224.1.0.1");
+	url->port = 11111;
+
+	return true;
 }
 
 bool url_open(struct url_t *url)
 {
-	return false;
+	url->socket = socket(url->network, url->protocol, 0);
+
+	return true;
+}
+
+bool url_close(struct url_t *url)
+{
+	close(url->socket);
+
+	return true;
 }
